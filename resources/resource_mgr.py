d759cbfacf15aa1c1931c6e60a2b1f681cf7f151
cohneli <elic@smartit.co.il>
2021-10-31 18:52:48 +0200

    check user name

--- resources/resource_mgr.py
+++ resources/resource_mgr.py
@@ -1,6 +1,8 @@
 def generate_resources():    
     return ['pic1.jpg','pic2.jpg','pic3.jpg']
 
-
+def check_user_name(user_name):
+    return len(user_name > 5) 
+    
 if __name__ == '__main__':
     print(generate_resources())
